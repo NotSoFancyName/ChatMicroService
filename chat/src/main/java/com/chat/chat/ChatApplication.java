2ab3d093243d8846ad5cf9f2275150a2e932d5e7
Volodymyr Pankin <vovan3819@gmail.com>
2018-12-05 00:30:19 +0200

    Add max extra lines to Main

--- chat/src/main/java/com/chat/chat/ChatApplication.java
+++ chat/src/main/java/com/chat/chat/ChatApplication.java
@@ -3,7 +3,11 @@ package com.chat.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChatApplication {
@@ -11,4 +15,14 @@ public class ChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
+	
+	@Configuration
+    @EnableWebMvc
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
+    }
 }
